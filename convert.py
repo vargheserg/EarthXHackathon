24e453ae7345dd71ac4b2228347160a83360002e
Varghese Rony George <varghese.rony.george@gmail.com>
2020-04-24 18:23:17 -0400

    d

--- convert.py
+++ convert.py
@@ -1,9 +1,5 @@
-import codecs
-import osm2geojson
+import requests
 
 with codecs.open('file.osm', 'r', encoding='utf-8') as data:
-    xml = data.read()
-
-geojson = osm2geojson.xml2geojson(xml)
-# >> ***REMOVED*** "type": "FeatureCollection", "features": [ ... ] ***REMOVED***
+    
 
