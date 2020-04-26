b3c9bd937e05f11ab9bc3408d0675f11e313a1a1
Uzair <uzairmahmed@gmail.com>
2020-04-26 04:50:58 -0400

    deleted buggy folder

--- roofdetector/rooftop_detection.py
+++ roofdetector/rooftop_detection.py
@@ -83,8 +83,8 @@ def draw_box(img, x1, y1, x2, y2):
   cv2.rectangle(image, start, end, 0, 2)
   cv2.imwrite("my.png",image)
 
-  cv2.imshow("lalala", image)
-  k = cv2.waitKey(0)
+  #cv2.imshow("lalala", image)
+  #k = cv2.waitKey(0)
 
 
 def get_roof_data(latitude, longitude):
