5e8666fa9bfe42da12380d2d755b509b52104e12
Varghese Rony George <varghese.rony.george@gmail.com>
2020-04-26 01:17:40 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,8 +1,7 @@
 from flask import Flask, request, jsonify
-from roof-detector import get_map
-from roof-detector import rooftop_detection
+from .roof-detector import get_map
+from .roof-detector import rooftop_detection
 import os
-
 app = Flask(__name__)
 
 @app.route('/')
